13800d56b1271678bc1e86527c280c676ef53441
Giedre Klimaite <giedre.klima@gmail.com>
2021-06-18 21:03:42 +0300

    add bootstrap, structure, data request

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,12 +1,43 @@
 import React from 'react';
+import Header from './Header';
+import AsideList from './AsideList';
 
 export default class Chat extends React.Component {
+state = {
+  contacts: []
+}
+
+componentDidMount() {
+let req = new XMLHttpRequest();
+
+req.onreadystatechange = () => {
+  if (req.readyState === XMLHttpRequest.DONE) {
+    //console.log(req.responseText);
+    let data = JSON.parse(req.responseText);
+    this.setState({contacts: data});
+    console.log(typeof data[0]);
+    console.log(data[0].users[0].name);
+    
+  }
+};
+
+req.open("GET", "https://api.jsonbin.io/b/60cc71a95ed58625fd138356", true);
+req.setRequestHeader("secret-key", "$2b$10$j9/f4SpIHRRyy1IfvpRCXuLEicKJINln.mw6UzvUwXiVrZy2gy3JK");
+req.send();
+        
+      }
 
   render () {
     return (
       <div>
-      <h1>Title</h1>
-      <p>subtite</p>
+        <Header />
+        <main>
+          <section>
+          </section>
+          
+          <AsideList />
+          
+        </main>
       </div>
     )
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Chat from './components/Chat';
+import 'bootstrap/dist/css/bootstrap.css';
 import './styles/styles.css';
 
 ReactDOM.render(<Chat />, document.getElementById('app'));
