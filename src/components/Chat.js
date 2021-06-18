da18dade3935d4dc06ca3250972ac1220177ec46
Giedre Klimaite <giedre.klima@gmail.com>
2021-06-18 15:19:02 +0300

    init

---
+++ src/components/Chat.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+export default class Chat extends React.Component {
+
+  render () {
+    return (
+      <div>
+      <h1>Title</h1>
+      <p>subtite</p>
+      </div>
+    )
+  }
+}
