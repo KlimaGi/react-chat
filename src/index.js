fe11b146fedf5516168c2b5c534b09e22d7a1797
Giedre Klimaite <giedre.klima@gmail.com>
2021-06-21 23:20:47 +0300

    linking between routes

--- src/index.js
+++ src/index.js
@@ -1,42 +1,62 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 //import Chat from './components/Chat';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { BrowserRouter, Route, Switch, Link, NavLink } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './styles/styles.css';
 
 const Login = () => (
   <div>
    This is from login page.
+   <Link to="chat">Chat</Link>
   </div>
 );
 
 const Acount = () => (
   <div>
   This is from Acount page
+    <p>
+      <Link to="chat">Chat</Link>
+    </p>
   </div>
 );
 
 const Chat = () => (
   <div>
-  This is chat page.
+    <p>
+        <NavLink to="/" activeClassName="is-active">Sign out</NavLink>
+        <NavLink to="/acount" activeClassName="is-active">Acount</NavLink>
+    </p>
+  
+      This is chat page.
   </div>
 );
 
 const NotFound = () => (
   <div>
-  404!
+      404! - <Link to="/">Go home</Link>
   </div>
+);
+
+const Headertop = () => (
+  <header>
+      <h1>Header</h1>
+      
+      <NavLink to="/acount" activeClassName="is-active">Acount</NavLink>
+  </header>
 );
 
 const routes = (
   <BrowserRouter>
-    <Switch>
-       <Route path="/" component={Login} exact={true} />
-       <Route path="/acount" component={Acount} />
-       <Route path="/chat" component={Chat} />
-       <Route component={NotFound} />
-    </Switch>
+  <div>
+      <Headertop />
+      <Switch>
+        <Route path="/" component={Login} exact={true} />
+        <Route path="/acount" component={Acount} />
+        <Route path="/chat" component={Chat} />
+        <Route component={NotFound} />
+      </Switch>
+    </div>
   </BrowserRouter>
 
 )
